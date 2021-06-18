9dc2eb08a6c30cce98329a80add02d2a84ac0a75
Balasainathreddy123 <84837747+Balasainathreddy123@users.noreply.github.com>
2021-06-18 20:38:24 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,28 +1,11 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
-/*max function*/
-int max(int a[],int n)
-{
-	int max=a[0];
-	for (int i=0;i<n;i++)
-	{
-		if (a[i]>max)
-			max=a[i];
-		else
-			continue;
-	}
-	return max;
-}
-/*min function*/
-int min(int a[],int n)
-{
-	int min=a[0];
-	for(int i=0;i<n;i++)
-	{
-		if(a[i]<min)
-			min=a[i];
-		else
-			continue;
-	}
-	return min;
-}
+/* max function*/
+int max(int arr[], int len){
+  int maximum = arr[0];
+  for(int i= 0; i< len; i++){
+    if(arr[i]> maximum)
+      maximum = arr[i];
+  }
+  return maximum;
+       }
