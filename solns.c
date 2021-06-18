4af6b4a084a712681176c5dcc8fbf3afaaedc29d
Balasainathreddy123 <84837747+Balasainathreddy123@users.noreply.github.com>
2021-06-18 20:33:53 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -13,3 +13,16 @@ int max(int a[],int n)
 	}
 	return max;
 }
+/*min function*/
+int min(int a[],int n)
+{
+	int min=a[0];
+	for(int i=0;i<n;i++)
+	{
+		if(a[i]<min)
+			min=a[i];
+		else
+			continue;
+	}
+	return min;
+}
